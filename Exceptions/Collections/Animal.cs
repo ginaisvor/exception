84da1139756fde7d4fc2bcdb0fbe75dcd6638321
ginaisvor <ginaisvor@yahoo.com>
2018-12-15 13:37:46 +0200

    initial commit

---
+++ Exceptions/Collections/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public Animal(string name, Gender gender)
+        {
+            this.Name = name;
+            this.Gender = gender;
+
+        }
+    }
+}
