cfb331f7a6e669822d957314ab952323b2e3cd16
ginaisvor <ginaisvor@yahoo.com>
2018-12-15 12:20:35 +0200

    initial commit

---
+++ Exceptions/Exceptions/DressCodeException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptions
+{
+    public class DressCodeException : ApplicationException
+    {
+       private readonly string message = "You are not wearing an Elf costume and you are underaged.";
+
+        public DressCodeException()
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+
+}
--- Exceptions/Exceptions/Program.cs
+++ Exceptions/Exceptions/Program.cs
@@ -7,6 +7,43 @@ namespace Exceptions
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            try
+            {
+                int a = 100;
+                int b = a / 0;
+                Console.WriteLine(b);
+            }
+            catch(DivideByZeroException exception)
+            {
+                Console.WriteLine("Oops, something happend!");
+                Console.WriteLine(exception.Message);
+                Console.WriteLine(exception.StackTrace);
+            }
+            Console.WriteLine("_______________________________________");
+            try
+            {
+                Person person1 = new Person(15);
+                //Person person2 = new Person(-5);
+                Person person3 = new Person(2);
+                Person person4 = new Person(83);
+            }
+            catch(ArgumentException e)
+            {
+                
+                //this will show all the error
+                //Console.WriteLine(e);
+                Console.WriteLine(e.Message + e.GetType());
+                
+
+            }
+
+            catch (DressCodeException f)
+            {
+                Console.WriteLine(f.Message + f.GetType());
+            }
+
+
+
         }
     }
 }
